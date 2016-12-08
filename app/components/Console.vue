4374ea75051aee614d0f41c455fbb62b58f6af45
Jen Kagan <kaganjd@gmail.com>
2016-12-07 22:34:39 -0500

    Styling packets in Console

--- app/components/Console.vue
+++ app/components/Console.vue
@@ -12,6 +12,62 @@
         Current Tool: {{ currentTool}} Current View: {{ currentView}}
         <sniffer v-if="currentTool === 'PcapSniffer'"
                  v-bind:packets="packets"> </sniffer>
+          <table class="packets-table">
+            <thead class="packets-table__header">
+              <tr>
+                <th scope="col">Number</th>
+                <th scope="col">Host Name</th>
+                <th scope="col">Packet Type</th>
+                <th scope="col">Source IP</th>
+                <th scope="col">Dest IP</th>
+              </tr>
+            </thead>
+            <tbody>
+                <tr class="packets-table__row">
+<!--                 hard-coded to test styling -->
+                  <td scope="row">Lorem ipsum</td>
+                  <td scope="row">www.duckduckgo.com</td>
+                  <td scope="row">TCP</td>
+                  <td scope="row">192.168.1.3</td>
+                  <td scope="row">192.168.1.4</td>
+                </tr>
+                <tr class="packets-table__row">
+                  <td scope="row">Lorem ipsum</td>
+                  <td scope="row">www.duckduckgo.com</td>
+                  <td scope="row">TCP</td>
+                  <td scope="row">192.168.1.3</td>
+                  <td scope="row">192.168.1.4</td>
+                </tr>
+                <tr class="packets-table__row">
+                  <td scope="row">Lorem ipsum</td>
+                  <td scope="row">www.duckduckgo.com</td>
+                  <td scope="row">TCP</td>
+                  <td scope="row">192.168.1.3</td>
+                  <td scope="row">192.168.1.4</td>
+                </tr>
+                <tr class="packets-table__row">
+                  <td scope="row">Lorem ipsum</td>
+                  <td scope="row">www.duckduckgo.com</td>
+                  <td scope="row">TCP</td>
+                  <td scope="row">192.168.1.3</td>
+                  <td scope="row">192.168.1.4</td>
+                </tr>
+                <tr class="packets-table__row">
+                  <td scope="row">Lorem ipsum</td>
+                  <td scope="row">www.duckduckgo.com</td>
+                  <td scope="row">TCP</td>
+                  <td scope="row">192.168.1.3</td>
+                  <td scope="row">192.168.1.4</td>
+                </tr>
+                <tr class="packets-table__row">
+                  <td scope="row">Lorem ipsum</td>
+                  <td scope="row">www.duckduckgo.com</td>
+                  <td scope="row">TCP</td>
+                  <td scope="row">192.168.1.3</td>
+                  <td scope="row">192.168.1.4</td>
+                </tr>
+            </tbody>
+          </table>
   </div>
 
 </template>

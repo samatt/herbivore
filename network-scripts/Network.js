ee929c9fe4b9f4d905c6f6d16971e110e0512966
Surya Mattu <surya@suryamattu.com>
2017-02-05 12:51:28 -0500

    standard js linting + hostname lookup refactor

--- network-scripts/Network.js
+++ network-scripts/Network.js
@@ -3,57 +3,57 @@ const ping = require('ping')
 const network = require('network')
 const oui = require('oui')
 const spawn = require('child-process-promise').spawn
-const exec = require('child_process').exec;
+const exec = require('child_process').exec
 
-class Network{
+class Network {
 
   constructor () {
     this.name = 'Network'
     this._client = null
     this.tbl = []
-    this.ips = []
+    this.unresolvedIps = []
     this.currentIndex = 0
-    this.public = ""
-    this.local = ""
-    this.gw = ""
-    this.subnet = ""
-    this.mac = ""
-    this.interface = ""
-    this.netmask = ""
-    this.type = ""
+    this.public = ''
+    this.local = ''
+    this.gw = ''
+    this.subnet = ''
+    this.mac = ''
+    this.interface = ''
+    this.netmask = ''
+    this.type = ''
+    this.hostInterval = null
   }
 
   info (msg) {
     console.log(`[${this.name}] : ${msg}`)
   }
 
-  error (msg){
+  error (msg) {
     console.error(`[${this.name}] Error : ${msg}`)
   }
 
-  set client(socket) {
+  set client (socket) {
     this._client = socket
   }
 
-  get client() {
+  get client () {
     return this._client
   }
 
-  init (){
-
-    if(this.local === ''){
+  init () {
+    this.checkHost()
+    if (this.local === '') {
       network.get_active_interface((err, obj) => {
-
-        if (!!err){
+        if (err) {
           let data = {iface: 'No active interface', connected: false}
-          this._client.emit('info', data);
+          this._client.emit('info', data)
           this.error('get_active_interface : ' + err.message)
           return
         }
 
-        if (!obj){
+        if (!obj) {
           let data = {iface: 'No active interface', connected: false}
-          this._client.emit('info', data);
+          this._client.emit('info', data)
           this.error('get_active_interface:  no interface found')
           return
         }
@@ -69,160 +69,126 @@ class Network{
         this.type = obj.type
 
         let data = { connected: true,
-                     private_ip: this.local,
-                     iface: this.interface,
-                     gateway: this.gw,
-                     netmask: this.netmask,
-                     mac: this.mac,
-                     type: this.type,
-                     vendor:this.vendor}
+          private_ip: this.local,
+          iface: this.interface,
+          gateway: this.gw,
+          netmask: this.netmask,
+          mac: this.mac,
+          type: this.type,
+          vendor: this.vendor}
 
         this._pingSubnet()
-        if(this._client){
-          this._client.emit('info', data);
-            try{
-              this._getHostName(this.local)
-            }
-            catch(err){
-              this.error(err)
-            }
 
-        }
-        else{
+        if (this._client) {
+          this._client.emit('info', data)
+          try {
+            this._getHostName(this.local)
+          } catch (err) {
+            this.error(err)
+          }
+        } else {
           this.error('Client socket not found!')
         }
-        if(this.tbl.length === 0 ){
-            try{
-              this._scanArpTable();
-            }
-            catch(err){
-              this.error(err)
-            }
 
+        if (this.tbl.length === 0) {
+          try {
+            this._scanArpTable()
+          } catch (err) {
+            this.error(err)
+          }
         }
 
-        if(this.public === ''){
-          network.get_public_ip( (err, ip) => {
-            if (!!err) return this.error(' get public ip: ' + err.message)
+        if (this.public === '') {
+          network.get_public_ip((err, ip) => {
+            if (err) return this.error(' get public ip: ' + err.message)
             if (!ip) return this.error(' get public ip:  no ip found')
             this.info(`public: ${ip} `)
             this.public = ip
-            if(this._client){
-              this._client.emit('updatePublicIp', this.public);
-            }
-            else{
+            if (this._client) {
+              this._client.emit('updatePublicIp', this.public)
+            } else {
               this.error('Client socket not found!')
             }
           })
         }
-
       })
     }
-
   }
 
   _getHostNamePromise (ip) {
     this.info(`Finding host for ip: ${ip}`)
     // TODO: Add linux version
-    spawn('dig', ['-x',ip,'-p','5353','@224.0.0.251'], { capture: [ 'stdout', 'stderr' ]})
+    spawn('dig', ['-x', ip, '-p', '5353', '@224.0.0.251'], {capture: [ 'stdout', 'stderr' ]})
     .then((result) => {
-      let text = result.stdout.toString();
+      let text = result.stdout.toString()
       let lines = text.split('\n')
       console.log(lines)
       let line = lines[11]
       let hostAdd = line.split('\t').pop()
       let hostname = hostAdd.split('.')[0]
       this.info(hostname)
-      this._client.emit('updateHostname', {ip: ip, hostname: hostname});
+      this._client.emit('updateHostname', {ip: ip, hostname: hostname})
     })
     .catch(function (err) {
-        this.error(err.stderr);
+      this.error(err.stderr)
     })
   }
 
   _getHostName (ip) {
     this.info(`Finding host for ip: ${ip}`)
     // TODO: Add linux version
-    exec(`dig -x ${ip} -p 5353 @224.0.0.251`,  (err, stdout, stderr) => {
-
-        if(err) { this.error(`err: ${ip} ${err} `); return}
-        if(stderr) {this.error(`err: ${ip} ${stderr} `); return }
-        let text = stdout;
-        let lines = text.split('\n')
-        let line = lines[11]
-        let hostAdd = line.split('\t').pop()
-        let hostname = hostAdd.split('.')[0]
-        this.info(hostname)
-        this._client.emit('updateHostname', {ip: ip, hostname: hostname});
+    exec(`dig -x ${ip} -p 5353 @224.0.0.251`, (err, stdout, stderr) => {
+      if (err) { this.error(`err: ${ip} ${err} `); return }
+      if (stderr) { this.error(`err: ${ip} ${stderr} `); return }
+      let text = stdout
+      let lines = text.split('\n')
+      let line = lines[11]
+      let hostAdd = line.split('\t').pop()
+      let hostname = hostAdd.split('.')[0]
+      this.info(`ip: ${ip} hostname: ${hostname}`)
+      this._client.emit('updateHostname', {ip: ip, hostname: hostname})
     })
   }
 
   _scanArpTable () {
-      arp.table( (err, entry) => {
-          if (!!err) return console.log('arp: ' + err.message);
-          if (!entry) return;
-          let t = entry.ip.split('.')
-          let lo = t.pop()
+    arp.table((err, entry) => {
+      if (err) return console.log('arp: ' + err.message)
+      if (!entry) return
+      let t = entry.ip.split('.')
+      t.pop()
 
-          if(this.interface !== entry.ifname){
-            this.info(`wrong interface: ${entry.ifname} ${this.interface} `);
-            return;
-          }
+      if (this.interface !== entry.ifname) {
+        this.info(`wrong interface: ${entry.ifname} ${this.interface} `)
+        return
+      }
 
-          if(entry.mac === "ff:ff:ff:ff:ff:ff"){
-            this.info(`ignore broadcast: ${entry.ifname} ${this.mac} `);
-            return;
-          }
+      if (entry.mac === 'ff:ff:ff:ff:ff:ff') {
+        this.info(`ignore broadcast: ${entry.ifname} ${this.mac} `)
+        return
+      }
 
-          if(t.join('.') !== this.subnet.join('.')){
-            this.info(`wrong subnet: ${t} ${this.subnet} `);
-            return;
-          }
-          this.tbl.push(entry)
+      if (t.join('.') !== this.subnet.join('.')) {
+        this.info(`wrong subnet: ${t} ${this.subnet} `)
+        return
+      }
+      this.tbl.push(entry)
 
-          this.info(`Found device: ${entry.mac} `)
-          if(this._client){
-            let vendor_info = oui(entry.mac)
-            if(vendor_info != null && vendor_info.indexOf('\n') > -1){
-              entry.vendor = vendor_info.split('\n')[0]
-            }
-            else{
-             entry.vendor = ''
-            }
+      this.info(`Found device: ${entry.mac} `)
+      if (this._client) {
+        let vendorInfo = oui(entry.mac)
+        if (vendorInfo != null && vendorInfo.indexOf('\n') > -1) {
+          entry.vendor = vendorInfo.split('\n')[0]
+        } else {
+          entry.vendor = ''
+        }
 
-            entry.hostname = ''
-            // this._getHostName(entry.ip)
-            this._client.emit('addNode', entry);
-            if(lo === '255'){
-              this.ips.push(entry.ip)
-                // Promise.all(this.ips.slice(0,20))
-                // .then( (result) => {
-                //   this.info(result)
-                //   result.forEach((r) => {
-                //     let text = r;
-                //     let lines = text.split('\n')
-                //     let line = lines[11]
-                //     let hostAdd = line.split('\t').pop()
-                //     let hostname = hostAdd.split('.')[0]
-                //     this.info(hostname)
-                //   })
-
-                // })
-                // .catch(function (err) {
-                //     console.error('[exec] stderr: ', err);
-                // })
-            }
-            else{
-              // this.ips.push(this.hostPromise(entry.ip))
-              this.ips.push(entry.ip)
-              this._getAllHostnames(0)
-            }
-
-          }
-          else{
-            this.error('Client socket not found!')
-          }
-      })
+        entry.hostname = ''
+        this._client.emit('addNode', entry)
+        this.unresolvedIps.push(entry.ip)
+      } else {
+        this.error('Client socket not found!')
+      }
+    })
   }
 
   _pingSubnet () {
@@ -231,35 +197,61 @@ class Network{
     for (var i = 0; i < 255; i++) {
       let n = this.subnet.concat(i)
       let toPing = n.join('.')
-      ping.sys.probe(toPing, function(isAlive){
-        var msg = isAlive ? 'host ' + toPing + ' is alive' : 'host ' + toPing + ' is dead';
-        if(isAlive){
-          this.info(msg);
+      ping.sys.probe(toPing, function (isAlive) {
+        var msg = isAlive ? 'host ' + toPing + ' is alive' : 'host ' + toPing + ' is dead'
+        if (isAlive) {
+          this.info(msg)
         }
-      });
+      })
     }
   }
 
-  _getAllHostnames(index) {
-    // (index >= this.ips.length ) {
-    //   this.info('Completed Hosts')
-    // }
-    // exec(`dig -x ${ip} -p 5353 @224.0.0.251`, (err, stdout, stderr) => {
-
-    // })
+  _getAllHostnames (index) {
+    if (index >= this.ips.length) {
+      this.info('Completed Hosts')
+    } else {
+      const ip = this.ips[index]
+      this.info(`Finding hostname for ${ip}`)
+      exec(`dig -x ${ip} -p 5353 @224.0.0.251`, (err, stdout, stderr) => {
+        if (err) { this.error(`err: ${ip} ${err} `); return }
+        if (stderr) { this.error(`err: ${ip} ${stderr} `); return }
+        let text = stdout
+        let lines = text.split('\n')
+        let line = lines[11]
+        let hostAdd = line.split('\t').pop()
+        let hostname = hostAdd.split('.')[0]
+        this.info(hostname)
+        this._client.emit('updateHostname', {ip: ip, hostname: hostname})
+        index = index + 1
+        this._getAllHostnames(index)
+      })
+    }
   }
-  // hostPromise (ip) {
-  //     return new Promise(
-  //         function (resolve, reject) {
-  //           exec(`dig -x ${ip} -p 5353 @224.0.0.251`,
-  //             (err, stdout, stderr) => {
-  //                     if (err || stderr) {
-  //                         reject(err);
-  //                     }
-  //                     resolve(stdout);
-  //                 });
-  //         });
-  // }
+
+  checkHost () {
+    this.hostInterval = setInterval(() => {
+      this.getHostBuffer()
+    }, 1000)
+  }
+
+  getHostBuffer () {
+    let bufferSize = 5
+    if (this.unresolvedIps.length < 1) {
+      // this.info(`No unresolved Ips`)
+      return
+    } else {
+      let cur = []
+      if (this.unresolvedIps.length > bufferSize) {
+        cur = this.unresolvedIps.splice(0, bufferSize)
+      } else {
+        cur = this.unresolvedIps.splice(0, this.unresolvedIps.length)
+      }
+      this.info(`Finding hostnames for: ${cur.length} devices and still have ${this.unresolvedIps.length} devices remaining`)
+      for (let c in cur) {
+        this._getHostName(cur[c])
+      }
+    }
+  }
   start (socket) {
   }
 
@@ -267,11 +259,7 @@ class Network{
   }
 
 }
-
 module.exports = Network
-
-
-
 // _scanLan () {
 //   const subnet = this.gw.split('.')
 //   subnet.pop()
--- network-scripts/PcapSniffer.js
+++ network-scripts/PcapSniffer.js
@@ -1,55 +1,52 @@
 const pcap = require('pcap')
-const fs = require('fs')
 const pcapFilters = require('./pcap-filters')
 const tlsClientHello = require('is-tls-client-hello')
 const sni = require('sni')
-//sudo sysctl -w net.inet.ip.forwarding=1
-//sudo sysctl -w net.inet.ip.fw.enable=1
-//https://www.npmjs.com/package/electron-sudo
+// sudo sysctl -w net.inet.ip.forwarding=1
+// sudo sysctl -w net.inet.ip.fw.enable=1
+// https://www.npmjs.com/package/electron-sudo
 class PcapSniffer {
 
   constructor () {
     this.name = 'PcapSniffer'
     this.sniff = false
     this._if = 'en0'
-    this.initComplete = false;
+    this.initComplete = false
     this._client = null
     this.arpInterval = null
     this.arpTarget = null
-    try{
+    try {
       this.session = pcap.createSession(this._if, pcapFilters.https)
-    }
-    catch(err){
+    } catch (err) {
       this.session = null
     }
   }
 
-  info (msg){
+  info (msg) {
     console.log(`[${this.name}] : ${msg}`)
   }
 
-  error (msg){
+  error (msg) {
     console.log(`[${this.name} - Err] : ${msg}`)
   }
 
-  set client(socket) {
+  set client (socket) {
     this._client = socket
   }
 
-  get client() {
+  get client () {
     return this._client
   }
 
-  init (){
-    if(this.session && !this.initComplete){
+  init () {
+    if (this.session && !this.initComplete) {
       this.session.on('packet', this._cb.bind(this))
       this.initComplete = true
     }
-    if(this.session === null){
-      if(this._client){
-        this._client.emit('bpfError');
-      }
-      else{
+    if (this.session === null) {
+      if (this._client) {
+        this._client.emit('bpfError')
+      } else {
         this.error('Client socket not found!')
       }
     }
@@ -65,21 +62,20 @@ class PcapSniffer {
   }
 
   startArpSpoof (params) {
-    if(!this.arpInterval){
+    if (!this.arpInterval) {
       this.arpInterval = setInterval(() => {
         this.arpPack(params.toTarget)
         this.arpPack(params.toRouter)
       }, 5000)
-    this.info(`Start arp spoof. target:${params.toRouter.src_ip}`)
-    this.arpTarget = params.toRouter.src_ip
-    }
-    else{
+      this.info(`Start arp spoof. target:${params.toRouter.src_ip}`)
+      this.arpTarget = params.toRouter.src_ip
+    } else {
       this.info(`Spoof already running. clearing current spoof and restarting`)
       clearInterval(this.arpInterval)
-        this.arpInterval = setInterval(() => {
+      this.arpInterval = setInterval(() => {
         this.arpPack(params.toRouter)
         this.arpPack(params.toTarget)
-        }, 5000)
+      }, 5000)
       this.arpInterval = null
       this.arpTarget = null
       this.startArpSpoof(params)
@@ -87,7 +83,7 @@ class PcapSniffer {
   }
 
   stopArpSpoof () {
-    if(!this.arpInterval){
+    if (!this.arpInterval) {
       this.info(`Stop arp spoof`)
       clearInterval(this.arpInterval)
       this.arpInterval = null
@@ -105,10 +101,10 @@ class PcapSniffer {
     }
 
     params.toTarget = {
-      src_ip:d.gw_ip,
-      src_mac:d.self_mac,
-      target_ip:d.target_ip,
-      target_mac:d.target_mac
+      src_ip: d.gw_ip,
+      src_mac: d.self_mac,
+      target_ip: d.target_ip,
+      target_mac: d.target_mac
     }
     this.startArpSpoof(params)
   }
@@ -116,52 +112,51 @@ class PcapSniffer {
 // src mac (your OWN mac address)
 // target ip : gw (the node youre telling)
 // target mac: gw (the node youre telling)
-
-  arpPack (config={ src_ip:"192.168.1.35",
-                    src_mac:"60:03:08:93:6b:ba",
-                    target_ip:"192.168.1.1",
-                    target_mac:"6c:72:20:6b:89:44"})
-  {
-    let pkt_config = {
-            'op': 'request',
-            'src_ip': config.src_ip,
-            'src_mac': config.src_mac,
-            'dst_ip': config.target_ip,
-            'dst_mac': config.target_mac
-            }
-
-    let pkt = {};
-    pkt.dst = this.mac_to_arr("ff:ff:ff:ff:ff:ff");
-    pkt.src = this.mac_to_arr(pkt_config.src_mac);
-
-    // ARP
-    pkt.ether_type = [0x08, 0x06];
-    pkt.hw_type = [0x00, 0x01];
-    pkt.proto_type = [0x08, 0x00];
-    pkt.hw_len = [0x06];
-    pkt.proto_len = [0x04];
-    pkt.op = [0x00, 0x02];
-
-    pkt.src_mac = this.mac_to_arr(pkt_config.src_mac);
-    pkt.src_ip = this.ip_to_arr(pkt_config.src_ip);
-    pkt.dst_mac = this.mac_to_arr(pkt_config.dst_mac);
-    pkt.dst_ip = this.ip_to_arr(pkt_config.dst_ip);
-
-    var x;
-    var pktArr = [];
-    for (x in pkt) {
-      pktArr = pktArr.concat(pkt[x]);
+// {src_ip: '192.168.1.35',
+//     src_mac: '60:03:08:93:6b:ba',
+//     target_ip: '192.168.1.1',
+//     target_mac: '6c:72:20:6b:89:44'}
+  arpPack (config) {
+    let pktConfig = {
+      'op': 'request',
+      'src_ip': config.src_ip,
+      'src_mac': config.src_mac,
+      'dst_ip': config.target_ip,
+      'dst_mac': config.target_mac
     }
 
-    var arpRequest = new Buffer(pktArr);
-    this.session.inject(arpRequest);
+    let pkt = {}
+    pkt.dst = this.macToArr('ff:ff:ff:ff:ff:ff')
+    pkt.src = this.macToArr(pktConfig.src_mac)
+
+    // ARP
+    pkt.ether_type = [0x08, 0x06]
+    pkt.hw_type = [0x00, 0x01]
+    pkt.proto_type = [0x08, 0x00]
+    pkt.hw_len = [0x06]
+    pkt.proto_len = [0x04]
+    pkt.op = [0x00, 0x02]
+
+    pkt.src_mac = this.macToArr(pktConfig.src_mac)
+    pkt.src_ip = this.ipToArr(pktConfig.src_ip)
+    pkt.dst_mac = this.macToArr(pktConfig.dst_mac)
+    pkt.dst_ip = this.ipToArr(pktConfig.dst_ip)
+
+    var x
+    var pktArr = []
+    for (x in pkt) {
+      pktArr = pktArr.concat(pkt[x])
+    }
+
+    var arpRequest = new Buffer(pktArr)
+    this.session.inject(arpRequest)
   }
 
   _cb (raw) {
     const packet = pcap.decode.packet(raw)
     const parsed = this._parse(packet, raw)
-    if(this._client && parsed){
-      this._client.emit('newPacket', parsed);
+    if (this._client && parsed) {
+      this._client.emit('newPacket', parsed)
     }
   }
 
@@ -171,75 +166,72 @@ class PcapSniffer {
     const ip = eth.payload
     const tcp = ip.payload
 
-    if( tcp.sport === 8443 ||
-        tcp.sport === 443  ||
-        tcp.dport === 443  ||
-        tcp.dport === 8443 ){
-      if(tcp.data){
-        if(tlsClientHello(tcp.data)){
-          return  { ts: ts, eth: eth, ip: ip, tcp: tcp, payload: {type:'https', host:sni(tcp.data)}}
+    if (tcp.sport === 8443 ||
+        tcp.sport === 443 ||
+        tcp.dport === 443 ||
+        tcp.dport === 8443) {
+      if (tcp.data) {
+        if (tlsClientHello(tcp.data)) {
+          return {ts: ts, eth: eth, ip: ip, tcp: tcp, payload: {type: 'https', host: sni(tcp.data)}}
         }
       }
       return false
     }
-    if(this.arpTarget){
+    // if(this.arpTarget){
 
-    }
-    if(!tcp.data){
-      return false;
+    // }
+    if (!tcp.data) {
+      return false
     }
 
     let r = tcp.data.toString('utf-8')
-    if(r.indexOf('Content-Length') === -1 &&
+    if (r.indexOf('Content-Length') === -1 &&
         r.indexOf('Host') === -1 &&
-        r.indexOf('Content-Type') === -1 ){
-      return false;
+        r.indexOf('Content-Type') === -1) {
+      return false
     }
 
     let httpr = r.split('\r\n')
-    try{
-      return  { ts: ts, eth: eth, ip: ip, tcp: tcp, payload: this.parseHTTP(httpr)}
-    }
-    catch(err){
+    try {
+      return {ts: ts, eth: eth, ip: ip, tcp: tcp, payload: this.parseHTTP(httpr)}
+    } catch (err) {
       this.error(err)
       return false
     }
   }
 
-  mac_to_arr(macAddr) {
-    var mac_arr = macAddr.split(':');
-    var x;
-    for (x in mac_arr) {
-      mac_arr[x] = '0x' + mac_arr[x];
+  macToArr (macAddr) {
+    var macArr = macAddr.split(':')
+    var x
+    for (x in macArr) {
+      macArr[x] = '0x' + macArr[x]
     }
-    return mac_arr;
+    return macArr
   }
 
-
-  ip_to_arr(ipAddr) {
-    var ip_arr = ipAddr.split('.');
-    var x;
-    for (x in ip_arr) {
-      ip_arr[x] = ip_arr[x];
+  ipToArr (ipAddr) {
+    var ipArr = ipAddr.split('.')
+    var x
+    for (x in ipArr) {
+      ipArr[x] = ipArr[x]
     }
-    return ip_arr;
+    return ipArr
   }
 
-  parseHTTP(headers){
+  parseHTTP (headers) {
     let packet = {}
     packet.http = true
     packet.host = ''
     let firstline = headers.shift()
-    if( firstline.indexOf('GET') > -1 ||
+    if (firstline.indexOf('GET') > -1 ||
         firstline.indexOf('POST') > -1 ||
-        firstline.indexOf('PUT') > -1){
+        firstline.indexOf('PUT') > -1) {
       let [verb, url, version] = firstline.split(' ')
       packet.type = 'request'
       packet.method = verb
       packet.url = url
       packet.version = version
-    }
-    else{
+    } else {
       let [version, code, status] = firstline.split(' ')
       packet.type = 'response '
       packet.code = code
@@ -250,27 +242,25 @@ class PcapSniffer {
     packet.headers = []
 
     for (var i = 0; i < headers.length; i++) {
-      if(headers[i] === ''){
-        break;
+      if (headers[i] === '') {
+        break
       }
 
       let header = headers[i].split(': ')
-      if(header.length <2 ){
+      if (header.length < 2) {
         continue
-      }
-      else{
-        if(header[0].indexOf('Host') > -1){
+      } else {
+        if (header[0].indexOf('Host') > -1) {
           packet.host = header[1]
         }
         packet.headers.push([header[0], header[1]])
       }
     }
 
-    let lastline =  headers.pop()
+    let lastline = headers.pop()
     packet.payload = lastline
     return packet
   }
-
 }
 
 module.exports = PcapSniffer

ecf2617cc3b542b5fc5801611238e1528562cffa
Surya Mattu <surya@suryamattu.com>
2017-02-05 13:08:47 -0500

    tidy + linting

--- network-scripts/ToolManager.js
+++ network-scripts/ToolManager.js
@@ -1,15 +1,11 @@
-const fs = require('fs');
-const path = require('path');
 const toolnames = require('./config')['tools']
-const PcapSniffer = require('./PcapSniffer');
-const Network = require('./Network');
-const Info = require('./Info');
+const PcapSniffer = require('./PcapSniffer')
+const Network = require('./Network')
 
-
-class ToolManager{
+class ToolManager {
 
   constructor () {
-    this._client = {id:'not connected'}
+    this._client = {id: 'not connected'}
     this._currentTool = null
     this.toolnames = toolnames
     this.tools = this.loadTools()
@@ -17,13 +13,13 @@ class ToolManager{
 
   loadTools () {
     let tools = []
-    let sniffer =  new PcapSniffer()
+    let sniffer = new PcapSniffer()
     let network = new Network()
-    let info = new Info()
+    // let info = new Info()
 
     tools.push(sniffer)
     tools.push(network)
-    tools.push(info)
+    // tools.push(info)
 
     return tools
   }
@@ -36,88 +32,80 @@ class ToolManager{
     console.info(`[ToolManager] : ${msg}`)
   }
 
-  registerClients(socket){
-    for(var tool of this.tools){
+  registerClients (socket) {
+    for (var tool of this.tools) {
       tool.client = socket
       this.info(`Registering ${tool.client.id} to ${tool.name}`)
     }
   }
 
-  set client(socket){
+  set client (socket) {
     this._client = socket
   }
 
-  get client(){
+  get client () {
     return this._client
   }
 
-  set currentTool(toolname){
-  this._currentTool = toolname
+  set currentTool (toolname) {
+    this._currentTool = toolname
   }
 
-  get currentTool(){
+  get currentTool () {
     return this._currentTool
   }
 
-listTools () {
+  listTools () {
     this._client.emit('listTools', this.toolnames)
-}
-
-load(toolname){
-   let t =  this.tools.find(function(tool){
-      return tool.name === toolname
-    })
-   this._currentTool = t
-   if(t === undefined){
-    this.error(`Not Loaded ${toolname}`)
-   }
-   else{
-      this.info(`Loaded ${toolname}`)
-      this.init()
-   }
   }
 
+  load (toolname) {
+    let t = this.tools.find(function (tool) {
+      return tool.name === toolname
+    })
+    this._currentTool = t
+    if (t === undefined) {
+      this.error(`Not Loaded ${toolname}`)
+    } else {
+      this.info(`Loaded ${toolname}`)
+      this.init()
+    }
+  }
 
   init () {
-    if(!this._currentTool || !this._client){
+    if (!this._currentTool || !this._client) {
       this.error(`Bad Init Attempted`)
       return false
-    }
-  else{
-    this._currentTool.init(this._client)
+    } else {
+      this._currentTool.init(this._client)
     }
   }
 
   start () {
-    if(!this._currentTool || !this._client){
+    if (!this._currentTool || !this._client) {
       this.error(`Bad Start Attempted`)
       return false
-    }
-    else{
+    } else {
       this._currentTool.start(this._client)
     }
   }
 
   stop () {
-    if(!this._currentTool || !this._client){
+    if (!this._currentTool || !this._client) {
       this.error(`Bad Stop Attempted`)
       return false
-    }
-    else{
+    } else {
       this._currentTool.stop(this._client)
     }
   }
 
   updateTarget (data) {
-    if(!this._currentTool || !this._client
-        || this.currentTool.name !== "PcapSniffer"){
+    if (!this._currentTool || !this._client || this.currentTool.name !== 'PcapSniffer') {
       this.error(`Cant sniff, probably because current tool is not sniffer`)
       return false
-    }
-    else{
+    } else {
       this._currentTool.updateTarget(this._client, data)
     }
   }
 }
-
 module.exports = ToolManager

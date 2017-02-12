8583559d209c87db88843898c9f8047c5fa12c4b
Surya Mattu <surya@suryamattu.com>
2017-02-12 18:47:39 -0500

    selectable text

--- app/components/tools/SnifferPayload.vue
+++ app/components/tools/SnifferPayload.vue
@@ -5,21 +5,21 @@
          <template v-if="packet.payload.type !== 'https'">
 
            <template v-if="packet.payload.type === 'request'">
-             <div><strong>Type</strong> : Request</div>
-             <div><strong>Method</strong> : {{packet.payload.method}}</div>
-             <div><strong>Url</strong>: {{packet.payload.host}}{{packet.payload.url}}</div>
+             <div class="selectable-text" ><strong>Type</strong> : Request</div>
+             <div class="selectable-text" ><strong>Method</strong> : {{packet.payload.method}}</div>
+             <div class="selectable-text" ><strong>Url</strong>: {{packet.payload.host}}{{packet.payload.url}}</div>
            </template>
 
            <template v-else>
-              <div><strong>Type</strong> : Response</div>
-             <div><strong>Response Code</strong> : {{packet.payload.code}} {{packet.payload.status}}</div>
+             <div class="selectable-text"><strong>Type</strong> : Response</div>
+             <div class="selectable-text" ><strong>Response Code</strong> : {{packet.payload.code}} {{packet.payload.status}}</div>
            </template>
 
          </template>
           <div v-else>
             This is an encrypted HTTPS packet for {{packet.payload.host}}
          </div>
-        <div v-for="p in packet.payload.headers">
+        <div class="selectable-text" v-for="p in packet.payload.headers">
           <strong>{{p[0]}}:</strong> {{p[1]}}
         </div>
      </template>
